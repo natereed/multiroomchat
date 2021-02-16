ae67360ef05b15b0278d9dda65b2301ebe06ce19
Nate <nate@natereed.com>
2021-02-16 16:02:49 -0600

    Handle multiple rooms

--- index.js
+++ index.js
@@ -3,22 +3,31 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-    res.render('index.ejs');
+app.get('/room/:id', function(req, res) {
+    id = req.params.id;
+    res.render('index.ejs', {
+        roomId: id
+    });
 });
 
 io.sockets.on('connection', function(socket) {
-    socket.on('username', function(username) {
+    socket.on('username', function(roomId, username) {
         socket.username = username;
-        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+        socket.roomId = roomId;
+        // Join room here:
+        socket.join(roomId);
+        io.to(roomId).emit('is_online', '🔵 <i>' + socket.username + ' joined ' + roomId + '.</i>');
     });
 
     socket.on('disconnect', function(username) {
-        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+        // Should the room id come from socket or from the emitted message (function arg)?
+        io.to(socket.roomId).emit('is_online', '🔴 <i>' + socket.username + ' left ' + socket.roomId + '.</i>');
+        // Leave room
+        socket.leave(socket.roomId);
     })
 
     socket.on('chat_message', function(message) {
-        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+        io.to(socket.roomId).emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
     });
 
 });
