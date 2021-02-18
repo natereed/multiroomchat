2705355d08bfe02695bda81094c3a0c95e810513
Nate <nate@natereed.com>
2021-02-17 19:21:28 -0600

    Enable CORS from localhost:3000

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require("socket.io")(http, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
 
 app.get('/room/:id', function(req, res) {
     id = req.params.id;
