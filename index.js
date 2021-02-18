a2fda48f7ee775cd6acb88730490735a81c36ae8
Mark Black <blackmarkt@gmail.com>
2021-02-18 08:11:00 -0800

    add gravatar

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ io.sockets.on('connection', function(socket) {
         socket.roomId = roomId;
         // Join room here:
         socket.join(roomId);
-        io.to(roomId).emit('is_online', '<i>' + socket.username + ' joined ' + roomId + '.</i>');
+        io.to(roomId).emit('is_online', '<span class="gravatar">👽</span><i>' + socket.username + ' joined ' + roomId + '.</i>');
     });
 
     socket.on('disconnect', function(username) {
         // Should the room id come from socket or from the emitted message (function arg)?
-        io.to(socket.roomId).emit('is_online', '<i>' + socket.username + ' left ' + socket.roomId + '.</i>');
+        io.to(socket.roomId).emit('is_online', '<span class="gravatar">👽</span><i>' + socket.username + ' left ' + socket.roomId + '.</i>');
         // Leave room
         socket.leave(socket.roomId);
     })
